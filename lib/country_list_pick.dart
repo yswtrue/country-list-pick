80605f13e44faa5db17eb1c8e9efe51e16d9aa15
yswtrue <a365039311@gmail.com>
2021-12-20 19:13:32 +0800

    change title

--- lib/country_list_pick.dart
+++ lib/country_list_pick.dart
@@ -83,7 +83,7 @@ class _CountryListPickState extends State<CountryListPick> {
             appBar: widget.appBar ??
                 AppBar(
                   backgroundColor: Theme.of(context).appBarTheme.color,
-                  title: Text("Select Country"),
+                  title: Text("Select Country/Region"),
                 ),
             theme: theme,
             countryBuilder: widget.countryBuilder,
